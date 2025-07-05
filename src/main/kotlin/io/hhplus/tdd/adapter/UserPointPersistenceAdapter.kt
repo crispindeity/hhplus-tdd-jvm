d4aece64c4f4cbb4a8bd7939d08a34c5fd508c74
crispin <h.c.shin.dev09@gmail.com>
2025-07-05 23:29:35 +0900

    Feat: 유저 포인트 내역 조회 어뎁터 추가
    Feat: implement user point persistence adapter
    
    Fix: add service annotation

--- src/main/kotlin/io/hhplus/tdd/adapter/UserPointPersistenceAdapter.kt
+++ src/main/kotlin/io/hhplus/tdd/adapter/UserPointPersistenceAdapter.kt
@@ -1,29 +1,33 @@
 package io.hhplus.tdd.adapter
 
+import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.UserPoint
 import io.hhplus.tdd.point.port.output.UserPointCommandPort
 import io.hhplus.tdd.point.port.output.UserPointQueryPort
 import org.springframework.stereotype.Component
 
 @Component
-class UserPointPersistenceAdapter :
-    UserPointQueryPort,
+class UserPointPersistenceAdapter(
+    private val userPointTable: UserPointTable
+) : UserPointQueryPort,
     UserPointCommandPort {
-    override fun findBy(id: Long): UserPoint? {
-        TODO("Not yet implemented")
-    }
+    override fun findBy(id: Long): UserPoint? = userPointTable.selectById(id)
 
     override fun chargeUserPoint(
         id: Long,
         amount: Long
-    ): UserPoint {
-        TODO("Not yet implemented")
-    }
+    ): UserPoint =
+        userPointTable.insertOrUpdate(
+            id = id,
+            amount = amount
+        )
 
     override fun useUserPoint(
         id: Long,
         amount: Long
-    ): UserPoint {
-        TODO("Not yet implemented")
-    }
+    ): UserPoint =
+        userPointTable.insertOrUpdate(
+            id = id,
+            amount = amount
+        )
 }
