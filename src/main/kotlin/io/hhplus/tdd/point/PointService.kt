105f394b19824cfbc97f4b0bfa702119671d8a37
crispin <h.c.shin.dev09@gmail.com>
2025-07-05 17:40:40 +0900

    Feat: add not found point history error code
    Feat: add point service extensions
    
    Feat: add retrieve poinst history
    
    Test: add retrieve point history test
    
    Test: add retrieve point history fail test

--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -2,14 +2,19 @@ package io.hhplus.tdd.point
 
 import io.hhplus.tdd.exception.CustomException
 import io.hhplus.tdd.exception.ErrorCode
+import io.hhplus.tdd.point.extensions.toDto
+import io.hhplus.tdd.point.port.input.PointHistoryQueryUseCase
 import io.hhplus.tdd.point.port.input.UserPointQueryUseCase
+import io.hhplus.tdd.point.port.output.PointHistoryQueryPort
 import io.hhplus.tdd.point.port.output.UserPointQueryPort
 import io.hhplus.tdd.util.Log
 import org.slf4j.Logger
 
 class PointService(
-    private val userPointQueryPort: UserPointQueryPort
-) : UserPointQueryUseCase {
+    private val userPointQueryPort: UserPointQueryPort,
+    private val pointHistoryQueryPort: PointHistoryQueryPort
+) : UserPointQueryUseCase,
+    PointHistoryQueryUseCase {
     private val logger: Logger = Log.getLogger(PointService::class.java)
 
     override fun retrieveUserPoint(id: Long): UserPointQueryUseCase.UserPointResponse =
@@ -26,6 +31,27 @@ class PointService(
                 id = foundUserPoint.id,
                 point = foundUserPoint.point,
                 updateMillis = foundUserPoint.updateMillis
+            )
+        }
+
+    override fun retrievePointHistory(
+        userId: Long
+    ): PointHistoryQueryUseCase.PointHistoryResponses =
+        Log.logging(logger) { log ->
+            log["method"] = "retrievePointHistory()"
+
+            if (!pointHistoryQueryPort.exists(userId)) {
+                throw CustomException(
+                    codeInterface = ErrorCode.NOT_FOUND_POINT_HISTORY,
+                    additionalMessage = userId.toString()
+                )
+            }
+
+            val pointHistories: List<PointHistory> = pointHistoryQueryPort.findBy(userId)
+
+            return@logging PointHistoryQueryUseCase.PointHistoryResponses(
+                responses = pointHistories.map { it.toDto() },
+                count = pointHistories.size
             )
         }
 }
--- src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
@@ -2,7 +2,9 @@ package io.hhplus.tdd.point
 
 import io.hhplus.tdd.exception.CustomException
 import io.hhplus.tdd.exception.ErrorCode
+import io.hhplus.tdd.fake.FakePointHistoryQueryPort
 import io.hhplus.tdd.fake.FakeUserPointQueryPort
+import io.hhplus.tdd.point.port.input.PointHistoryQueryUseCase
 import io.hhplus.tdd.point.port.input.UserPointQueryUseCase
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.SoftAssertions
@@ -14,13 +16,16 @@ import org.junit.jupiter.api.Test
 class PointServiceTest {
     private lateinit var pointService: PointService
     private lateinit var fakeUserPointQueryPort: FakeUserPointQueryPort
+    private lateinit var fakePointHistoryQueryPort: FakePointHistoryQueryPort
 
     @BeforeEach
     fun setUp() {
         fakeUserPointQueryPort = FakeUserPointQueryPort()
+        fakePointHistoryQueryPort = FakePointHistoryQueryPort()
         pointService =
             PointService(
-                userPointQueryPort = fakeUserPointQueryPort
+                userPointQueryPort = fakeUserPointQueryPort,
+                pointHistoryQueryPort = fakePointHistoryQueryPort
             )
     }
 
@@ -67,6 +72,59 @@ class PointServiceTest {
                         pointService.retrieveUserPoint(wrongUserId)
                     }.isInstanceOf(CustomException::class.java)
                     .hasMessage("${ErrorCode.NOT_FOUND_USER_POINT.message} - $wrongUserId")
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("포인트 히스토리 조회 테스트")
+    inner class RetrievePointHistoryTest {
+        @Nested
+        @DisplayName("포인트 히스토리 조회 성공 테스트")
+        inner class RetrievePointHistorySuccessTest {
+            @Test
+            @DisplayName("유저 아이디를 통해 해당 유저의 포인트 히스토리를 조회할 수 있어야 한다.")
+            fun retrievePointHistoryTest() {
+                // given
+                val userId = 1L
+
+                fakePointHistoryQueryPort.singlePointHistoryFixture(
+                    id = 1L,
+                    userId = userId
+                )
+
+                // when
+                val actual: PointHistoryQueryUseCase.PointHistoryResponses =
+                    pointService.retrievePointHistory(userId)
+
+                // then
+                SoftAssertions.assertSoftly { softAssertions ->
+                    softAssertions.assertThat(actual).isNotNull
+                    softAssertions.assertThat(actual.responses.first().userId).isEqualTo(userId)
+                }
+            }
+        }
+
+        @Nested
+        @DisplayName("포인트 히스토리 조회 실패 테스트")
+        inner class RetrievePointHistoryFailTest {
+            @Test
+            @DisplayName("존재하지 않는 유저 아이디로 포인트 히스토리 조회 시 예외가 발생해야 한다.")
+            fun retrievePointHistoryTest() {
+                // given
+                val wrongUserId = 1L
+
+                fakePointHistoryQueryPort.singlePointHistoryFixture(
+                    id = 1L,
+                    userId = 2L
+                )
+
+                // when & then
+                Assertions
+                    .assertThatThrownBy {
+                        pointService.retrievePointHistory(wrongUserId)
+                    }.isInstanceOf(CustomException::class.java)
+                    .hasMessage("${ErrorCode.NOT_FOUND_POINT_HISTORY.message} - $wrongUserId")
             }
         }
     }
