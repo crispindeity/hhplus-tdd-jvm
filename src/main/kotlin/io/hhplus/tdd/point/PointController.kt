e4f2a05d9b306d65b488ebdfc551c1d3b1215b73
crispin <h.c.shin.dev09@gmail.com>
2025-07-10 00:36:56 +0900

    Remove: 불필요한 코드 제거

--- src/main/kotlin/io/hhplus/tdd/point/PointController.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointController.kt
@@ -21,11 +21,6 @@ class PointController(
     private val pointHistoryQueryUseCase: PointHistoryQueryUseCase,
     private val userPointCommandUseCase: UserPointCommandUseCase
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(javaClass)
-
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}")
     fun point(
         @PathVariable id: Long
@@ -36,9 +31,6 @@ class PointController(
         return PointResponse.success(result = response)
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}/histories")
     fun history(
         @PathVariable id: Long
@@ -49,9 +41,6 @@ class PointController(
         return PointResponse.success(result = response)
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/charge")
     fun charge(
         @PathVariable id: Long,
@@ -66,9 +55,6 @@ class PointController(
         return PointResponse.success(result = response)
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/use")
     fun use(
         @PathVariable id: Long,
